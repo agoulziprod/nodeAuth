6a133d3f7bf724c767c8b727b24388f5d48c333a
AGOULZI mohamed <mohammed.agoulzi50@gmail.com>
2019-05-31 13:04:39 +0000

    done server validating data

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 module.exports = {
 
     signUp: async (req, res, next) => {
+        console.log(`content of req value body ${JSON.stringify(req.value.body)}`)
         console.log('tfaaa7 dial signUp')
     },
     signIn: async (req, res, next) => {
