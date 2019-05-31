35184e9d38a5cc8129d39cbaa06de077ea0ef2bf
AGOULZI mohamed <mohammed.agoulzi50@gmail.com>
2019-05-31 12:52:43 +0000

    done validating data of signup

--- helpers/routeHelpers.js
+++ helpers/routeHelpers.js
@@ -1,5 +1,25 @@
-const joi = require('joi')
+const Joi = require('joi')
 
 module.exports = {
-    validateBody: (schema)=> ef
+    validateBody: (schema) => {
+        return (req, res, next) => {
+            const result = Joi.validate(req.body, schema);
+            if (result.error) {
+                return res.status(400).json(result.error);
+            }
+// check if the req.value isnt theire
+            if (!req.value) {
+                req.value={}
+            }
+            // req.value.body instead req.body
+            req.value['body'] = result.value;
+            next();
+        }
+    },
+    schemas: {
+        authSchema: Joi.object().keys({
+            email: Joi.string().email().required(),
+            password: Joi.string().required()
+        })
+    }
 }
